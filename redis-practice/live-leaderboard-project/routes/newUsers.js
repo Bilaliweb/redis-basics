66079e61360db9630f988fd5c009b9898dd91e40
Muhammad Bilal Shahid <muhammadbilalshahid@192.168.10.4>
2026-07-16 23:51:45 +0500

    Ph-2 includes new user routes, new controller for views increment and rest is in progress

--- redis-practice/live-leaderboard-project/routes/newUsers.js
+++ redis-practice/live-leaderboard-project/routes/newUsers.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getAllUsersFromRedis, getNewSeededUserData, newUsersForRedis } from "../controllers/newUsers.js";
+import { getAllUsersFromRedis, getNewSeededUserData, increaseUserViewCount, newUsersForRedis } from "../controllers/newUsers.js";
 
 export const newUsersRouter = Router()
 
 newUsersRouter.get('/', getAllUsersFromRedis)
 newUsersRouter.get('/:id', getNewSeededUserData)
 newUsersRouter.post('/create', newUsersForRedis)
+newUsersRouter.post('/user/:id/view', increaseUserViewCount)
